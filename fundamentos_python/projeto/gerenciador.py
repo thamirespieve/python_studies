fd20a3deb190da29adbe61f3e8ab8177a32c2b6d
ThamiresP <thamires@4intelligence.com.br>
2025-08-12 09:00:35 -0300

    finish all crud functions

--- fundamentos_python/projeto/gerenciador.py
+++ fundamentos_python/projeto/gerenciador.py
@@ -15,6 +15,41 @@ def show_task(task_list):
         print(f"{index}. [{status}] {task_name}")
     return
 
+def update_task(index,updted_task,task_list):
+    correct_index = int(index)-1
+    if correct_index >=0 and correct_index < len(task_list):
+       task_list[correct_index]["tarefa"] = updted_task
+       print("Tarefa atualizada com sucesso!")
+    else:
+        print("Tarefa escolhida não existe")
+    
+    return
+
+def complete_taks(index,task_list):
+    correct_index= int(index)-1
+    if correct_index >=0 and correct_index < len(task_list):
+        task_list[correct_index]["completada"] = True
+        print(f"Tarefa {index} completada com sucesso!")
+    else:
+      print("Tarefa escolhida não existe")
+    return
+    
+def delete_task(index,task_list):
+    correct_index = int(index) - 1
+    if correct_index >=0 and correct_index < len(task_list):
+        task_list.pop(correct_index)
+        print(f"Tarefa {index} removida da lista.")
+    else:
+        print("Tarefa escolhida não existe")
+    return
+
+def delete_complete_task(task_list):
+    for task in task_list:
+        if task["completada"]:
+            task_list.remove(task)
+    print('Tarefas completadas foram deletadas')
+    return
+
 task_list =[]
 
 while True:
@@ -23,16 +58,33 @@ while True:
     print("2. Ver tarefas")
     print("3. Atualizar tarefa")
     print("4. Completar tarefa")
-    print("5. Deletar tarefas completadas")
-    print("6. Sair")
+    print("5. Deletar tarefas")
+    print("6. Deletar tarefas completadas")
+    print("7. Sair")
 
     escolha = input("Digite sua escolha:")
 
     if escolha == '1':
         task_name = input("Digite o nome da tarefa? ")
         add_task(task_name,task_list)
-    if escolha == '2':
+    elif escolha == '2':
         show_task(task_list)
+    elif escolha == '3':
+        show_task(task_list)
+        index = input("Qual número da tarefa que deseja atualizar?")
+        new_task = input("Qual a nova tarefa ?")
+        update_task(index,updted_task=new_task,task_list=task_list)    
+    elif escolha =='4':
+        show_task(task_list)
+        index= input("Qual o número da tarefa a ser completada ?")
+        complete_taks(index,task_list)
+    elif escolha == '5':
+        show_task(task_list)
+        index = input("Qual o número da tarefa que deseja excluir?")
+        delete_task(index,task_list)
     elif escolha == '6':
+        delete_complete_task(task_list)
+        show_task(task_list)
+    elif escolha == '7':
         break
 

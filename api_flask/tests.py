259327ccd6686bcad497bdb3e8566a5b8fd37584
ThamiresP <thamires@4intelligence.com.br>
2025-08-14 09:13:09 -0300

    feat: update and delete test

--- api_flask/tests.py
+++ api_flask/tests.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+#pytest tests.py -v
 
 #CRUD
 BASE_URL= 'http://127.0.0.1:5000'
@@ -18,7 +19,6 @@ def test_create_task():
     assert "id" in response_json
     tasks.append(response_json['id'])
 
-
 def test_get_tasks():
     response = requests.get(f"{BASE_URL}/tasks")
     assert response.status_code == 200
@@ -33,4 +33,36 @@ def test_get_task():
         assert response.status_code == 200
         response_json = response.json()
         assert response_json['id'] == task_id
+
+def test_update_task():
+    if tasks:
+        task_id=tasks[0]
+        payload = {
+            "completed": False,
+            "description":"Nova decrição",
+            "title": "Título atualizado"
+        }
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}",json=payload)
+        assert response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+
+        #Nova req a tarefa especifica
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}",json=payload)
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["title"] == payload["title"]
+        assert response_json["description"] == payload["description"]
+        assert response_json["completed"] == payload["completed"]
+
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404
 
